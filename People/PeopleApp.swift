659dbd4a733c1d702230b74f8e9d51cc645b7a87
Charlie <charlietunguyen@hotmail.com>
2023-06-21 22:50:49 +0100

    Home People UI

--- People/PeopleApp.swift
+++ People/PeopleApp.swift
@@ -1,17 +1,16 @@
-//
-//  PeopleApp.swift
-//  People
-//
-//  Created by Charlie Nguyen on 21/06/2023.
-//
-
 import SwiftUI
 
 @main
 struct PeopleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+			TabView {
+				PeopleView()
+					.tabItem {
+						Symbols.person
+						Text("Home")
+					}
+			}
         }
     }
 }

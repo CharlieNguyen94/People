c20b4d20b79aa81215dc969fa7fbff9208245fd4
Charlie <charlietunguyen@hotmail.com>
2023-06-21 17:56:19 +0100

    Initial Commit

---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by Charlie Nguyen on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

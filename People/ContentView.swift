e9552d5db288d66bb3943124d351ad948cff6841
Charlie <charlietunguyen@hotmail.com>
2023-06-21 18:14:05 +0100

    Project set up with symbols and colors

--- People/ContentView.swift
+++ People/ContentView.swift
@@ -1,16 +1,9 @@
-//
-//  ContentView.swift
-//  People
-//
-//  Created by Charlie Nguyen on 21/06/2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+		VStack {
+			Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
---
+++ People/Resources/Theme.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+enum Theme {
+	static let background = Color("background")
+	static let detailBackground = Color("detail-background")
+	static let text = Color("text")
+	static let pill = Color("pill")
+}
+
